386c62a2ebd9f898f68b1de0245eb9f860a3e15b
ywu256 <ywu256@my.centennialcollege.ca>
2025-03-11 22:57:19 -0400

    Add navigation and main screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,10 @@ android {
 
 dependencies {
 
+    implementation("androidx.compose.ui:ui:1.5.4")
+    implementation("androidx.compose.material:material:1.5.4")
+    implementation("androidx.compose.animation:animation:1.5.4")
+    implementation("androidx.navigation:navigation-compose:2.7.5")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
--- app/src/main/java/com/yulingwu/mapd721_assignment3/MainActivity.kt
+++ app/src/main/java/com/yulingwu/mapd721_assignment3/MainActivity.kt
@@ -4,13 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.yulingwu.mapd721_assignment3.ui.theme.MAPD721_assignment3Theme
 
 class MainActivity : ComponentActivity() {
@@ -19,16 +31,39 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MAPD721_assignment3Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                Navigation()
             }
         }
     }
 }
+
+@Composable
+fun MainScreen(navController: NavController) {
+    Column(
+        modifier = Modifier.fillMaxSize().padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Button(onClick = { navController.navigate("screen1") }) { Text("Screen 1: Animated Content") }
+        Button(onClick = { navController.navigate("screen2") }) { Text("Screen 2: Value-Based Animation 1") }
+        Button(onClick = { navController.navigate("screen3") }) { Text("Screen 3: Value-Based Animation 2") }
+        Button(onClick = { navController.navigate("screen4") }) { Text("Screen 4: Gesture-Based Animation") }
+
+        Spacer(modifier = Modifier.height(300.dp))
+
+        Text(text = "Student Name: Yu-Ling Wu", fontSize = 18.sp)
+        Text(text = "Student ID: 301434538", fontSize = 18.sp)
+    }
+}
+
+@Composable
+fun Navigation() {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = "mainScreen") {
+        composable("mainScreen") { MainScreen(navController) }
+    }
+}
+
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
